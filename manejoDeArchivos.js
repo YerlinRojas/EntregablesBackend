6ad7950a890ea5ca8f98fb7819430b9270e7fd57
Yerlin ROJAS <yerlinrojas808@gmail.com>
2023-06-09 17:24:27 -0300

    Manejo de archivos productManager

--- manejoDeArchivos.js
+++ manejoDeArchivos.js
@@ -14,99 +14,124 @@ Se llamará al método “updateProduct” y se intentará cambiar un campo de a
 Se llamará al método “deleteProduct”, se evaluará que realmente se elimine el producto o que arroje un error en caso de no existir.
  */
 
-const fs = require ('fs')
+const fs = require('fs')
 
 class ProductManager {
-    //1
-    constructor (path){
-        this.path = path
-        this.format = 'utf-8'
-    }
-    //2
-    getProduct= () => {
-        if (fs.existsSync(this.path)) {
-         return fs.promises.readFile(this.path, this.format)
-            .then(r => JSON.parse(r));
-        } else {
-          return [];
-        }
-      }
-    //3
-    addProduct = async(title,
-        description ,
-        price,
-        thumbnail ,
-        code ,
-        stock)=>{
-            const product = {
-                id: this.getId(),
-                title,
-                description ,
-                price,
-                thumbnail ,
-                code ,
-                stock}
-              
-                const listProduct = await this.getProduct()
-
-                if (listProduct.some(p => p.code === product.code)) {
-                console.log(`Ya existe un producto con el código ${product.code}`);
-                return null;
-                    }
-
-                listProduct.push(product);
-
-                await fs.promises.writeFile(this.path, JSON.stringify(listProduct));
-
-                console.log('Producto agregado correctamente.');
-                return product;
-                }
-    //4          
-
-    getId = ()=>{
-      const count = this.getProduct.length
-      if (count> 0) {
-        return this.product[count -1].id + 1
-      } else {
-        return 1
-      }
-    }
-
- //manera async PRoblema para generar ID 
-getId = async() => {
-    const listProduct = await this.getProduct()
-    const lastProduct = listProduct >0 ? lastProduct[listProduct.length -1].id : 0
-    return lastProduct +1
-}        
-
-  getProductById(id){
-      return this.getProduct()
-              .then(listProduct => {
-                const product = listProduct.find(p => p.id === id);
-                return product ? product : null;
-              })
-              .catch(error => {
-                console.error('Ocurrió un error al obtener los productos:', error);
-                return null;
-              })
+  constructor(path) {
+    this.path = path;
+    this.format = 'utf-8'
   }
- //manera async del getProductsById problemas con el tiempo de ejecucion entre ambas funciones, siempre da resultado NULL
+
+  getProduct = () => {
+    if (fs.existsSync(this.path)) {
+      return fs.promises.readFile(this.path, this.format).then(r => JSON.parse(r))
+    } else {
+      return []
+    }
+  };
+
+  addProduct = async (title, description, price, thumbnail, code, stock) => {
+    const product = {
+      id: await this.getId(),
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+
+    const listProduct = await this.getProduct()
+
+    if (listProduct.some(p => p.code === product.code)) {
+      console.log(`Ya existe un producto con el código ${product.code}`)
+      return null
+    }
+
+    listProduct.push(product);
+
+    await fs.promises.writeFile(this.path, JSON.stringify(listProduct))
+
+    console.log('Producto agregado correctamente.')
+    return product;
+  }
+
+  getId = async () => {
+    const listProduct = await this.getProduct()
+    const lastProduct = listProduct.length > 0 ? listProduct[listProduct.length - 1].id : 0
+    return lastProduct + 1
+  }
+
+  getProductById(id) {
+    return this.getProduct()
+      .then(listProduct => {
+        const product = listProduct.find(p => p.id === id)
+        return product ? product : null
+      })
+      .catch(error => {
+        console.error('Ocurrió un error al obtener los productos:', error)
+        return null
+      });
+  }
+
+  updateProduct = async (id, updatedData) => {
+    const listProduct = await this.getProduct()
+    const productIndex = listProduct.findIndex(product => product.id === id)
+
+    if (productIndex >0) {
+      listProduct[productIndex] = {
+        ...listProduct[productIndex],
+        ...updatedData,
+      }
+
+      await fs.promises.writeFile(this.path, JSON.stringify(listProduct))
+
+      console.log('Producto actualizado correctamente.')
+      return listProduct[productIndex]
+    } else {
+      console.log('Producto no encontrado.')
+      return null
+    }
+  }
+
+  deleteProduct = async(id) => {
+    const listProduct = await this.getProduct()
+    const productIndex = listProduct.findIndex(product => product.id === id)
+
+    if(productIndex >= 0) {
+    listProduct.splice(productIndex, 1)
+    await fs.promises.writeFile(this.path, JSON.stringify(listProduct))
+    console.log("id producto eliminado:", productIndex +1) 
+    return true
+     
+}else{
+  console.log("no se encontro el producto a eliminar")
+  return false
+}
+ 
 
 
-    updateProduct= () => {}
-    deleteProduct= () => {}
+  };
 }
 
-const functionAsync = async() => {
-const manager = new ProductManager('product.json')
- manager.addProduct("pruuba 1","prueb1", 23,"img",444,555)
- manager.addProduct("pruuba 2","prueb2", 23,"img",445,555)
+const functionAsync = async () => {
+  const manager = new ProductManager('product.json');
+  await manager.addProduct('prueba 1', 'prueba1', 23, 'img', 444, 555);
+  await manager.addProduct('prueba 2', 'prueba2', 23, 'img', 445, 555);
 
-/* console.log(await manager.getProduct())  */
-console.log(await manager.getId())
-console.log("este es el id", await manager.getProductById(1))
+  console.log("Lista de productos: ", await manager.getProduct())
+  console.log('Este es el producto: ', await manager.getProductById(1));
 
-}
+  const idAct = 2;
+  const productUpdateData = {
+    price: 5000,
+    code: 500,
+  };
+  console.log(await manager.updateProduct(idAct, productUpdateData));
 
+const productoEliminar = 1
+console.log(await manager.deleteProduct(productoEliminar))
 
-functionAsync ()
+};
+
+functionAsync();
